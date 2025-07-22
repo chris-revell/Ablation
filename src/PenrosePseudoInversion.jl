ef539aab1c781c2b1c2470a6f6cdb1fcfbd5c7f8
Christopher Revell <christopher.revell@manchester.ac.uk>
2025-07-22 13:41:07 +0100

    Update PenrosePseudoInversion.jl

--- src/PenrosePseudoInversion.jl
+++ src/PenrosePseudoInversion.jl
@@ -2,7 +2,7 @@
 #  File: PenrosePseudoInversion.jl
 #  Module: PenrosePseudoInversion
 #  Package: Ablation 
-#  Functions: inProd, penrosePseudoInversion
+#  Functions: innerProd, penrosePseudoInversion
 
 module PenrosePseudoInversion
 
@@ -14,35 +14,33 @@ using LinearAlgebra
 # Local modules
 # @from "$(srcdir("X.jl"))" using X
 
-inProd(a, M, b) = transpose(a) * M * b # Inner product of a and b under metric M
-
 function penrosePseudoInversion(L, g, M)
     n = length(g)
     λᵛ = (eigen(Matrix(L))).values
     eᵛs = (eigen(Matrix(L))).vectors
     eᵛ = [eᵛs[:, k] for k = 1:n] # Vector of eigenvectors
     𝟙 = ones(n)
-    ḡ = 𝟙 .* inProd(𝟙, M, g) / inProd(𝟙, M, 𝟙)
+    ḡ = 𝟙 .* innerProd(𝟙, M, g) / innerProd(𝟙, M, 𝟙)
     ğ = g .- ḡ
 
     ϕ̆ = zeros(n)
     ϕ̆Spectrum = Float64[]
     ϕ̄₀ = zeros(n)
     for k = 2:n
-        amplitude = inProd(eᵛ[k], M, ğ) / (λᵛ[k] * inProd(eᵛ[k], M, eᵛ[k]))
+        amplitude = innerProd(eᵛ[k], M, ğ) / (λᵛ[k] * innerProd(eᵛ[k], M, eᵛ[k]))
         ϕ̆ .+= amplitude .* eᵛ[k]
         push!(ϕ̆Spectrum, amplitude)
 
-        # ϕ̄₀ .+= eᵛ[k] .* inProd(eᵛ[k], M, 𝟙) / (λᵛ[k] * inProd(eᵛ[k], M, eᵛ[k]))
+        # ϕ̄₀ .+= eᵛ[k] .* innerProd(eᵛ[k], M, 𝟙) / (λᵛ[k] * innerProd(eᵛ[k], M, eᵛ[k]))
     end
 
-    # ϕ̄ = ϕ̄₀ * inProd(𝟙, M, g) / inProd(𝟙, M, 𝟙)
+    # ϕ̄ = ϕ̄₀ * innerProd(𝟙, M, g) / innerProd(𝟙, M, 𝟙)
     ϕ = ϕ̆ #.+ ϕ̄
 
     return ϕ, ϕ̆Spectrum
 end
 
-export inProd
+export innerProd
 export penrosePseudoInversion
 
 end
